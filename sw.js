a1d6f10c5c7884b1dc1893b4449a9ac545e9314d
mnacik1988 <mnacik1988@gmail.com>
2026-07-09 23:27:39 +0300

    Добавлен ID устройства в настройках для выдачи VIP-лимита, версия 0.24

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-var CACHE = 'vtodo-shell-v23';
+var CACHE = 'vtodo-shell-v24';
 var SHELL = ['./index.html', './manifest.json', './icon.png', './icon-maskable.png', './sw.js'];
 
 /* ── Install: кешируем приложение ── */
