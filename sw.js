78fe3ef3f25e11daa50f6eb1e8233e576cfe9129
mnacik1988 <mnacik1988@gmail.com>
2026-07-10 12:31:52 +0300

    Аудит-правки: полная очистка данных, убрано двойное уведомление, theme-color под манифест, чистка клиента

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-var CACHE = 'vtodo-shell-v27';
+var CACHE = 'vtodo-shell-v28';
 var SHELL = ['./index.html', './manifest.json', './icon.png', './icon-maskable.png', './sw.js'];
 
 /* ── Install: кешируем приложение ── */
