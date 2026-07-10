dfd76ab0206be8d4f488e601df9d10ad9e20879c
mnacik1988 <mnacik1988@gmail.com>
2026-07-10 10:54:03 +0300

    Фикс: убрано противоречивое 'задач нет' при просроченных задачах; убрано двойное мигание панели настроек

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-var CACHE = 'vtodo-shell-v25';
+var CACHE = 'vtodo-shell-v26';
 var SHELL = ['./index.html', './manifest.json', './icon.png', './icon-maskable.png', './sw.js'];
 
 /* ── Install: кешируем приложение ── */
