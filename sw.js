fef93902a397b093ef3db08dbda696c86389abc4
mnacik1988 <mnacik1988@gmail.com>
2026-07-16 16:33:59 +0300

    Добавить ручной переключатель ИИ-разбора голоса (экономия лимита) (v0.71)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-var CACHE = 'vtodo-shell-v70';
+var CACHE = 'vtodo-shell-v71';
 var SHELL = ['./index.html', './manifest.json', './icon.png', './icon-maskable.png', './apple-touch-icon.png', './sw.js'];
 
 /* ── Install: кешируем приложение ── */
