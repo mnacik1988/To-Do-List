7c2d322eb95f4bd0a484af726c7a6f71c00867fd
mnacik1988 <mnacik1988@gmail.com>
2026-07-17 18:42:40 +0300

    Добавить разбиение названия задачи на чек-лист через ИИ (v0.77)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-var CACHE = 'vtodo-shell-v76';
+var CACHE = 'vtodo-shell-v77';
 var SHELL = ['./index.html', './manifest.json', './icon.png', './icon-maskable.png', './apple-touch-icon.png', './sw.js'];
 
 /* ── Install: кешируем приложение ── */
